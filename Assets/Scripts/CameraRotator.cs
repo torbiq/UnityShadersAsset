34d8eefdb151ae824d4f25dd86c224977e27f957
Andrew Torbenko <andreytorbenko@gmail.com>
2017-08-06 20:34:54 +0300

    Finished gold shader with 3 materials

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotator : MonoBehaviour {
+
+    private Vector2 _prevMousePos,
+        _currMousePos;
+
+    private float _rotationSpeed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            _currMousePos = Input.mousePosition;
+        }
+	    if (Input.GetMouseButton(0)) {
+            _prevMousePos = _currMousePos;
+            _currMousePos = Input.mousePosition;
+
+            Vector3 delta = _currMousePos - _prevMousePos;
+            delta.z = 0;
+            Vector3 rotationAngles = new Vector3(-delta.y, delta.x, delta.z);
+
+            Vector3 final = _rotationSpeed * rotationAngles;
+
+            Camera.main.transform.parent.Rotate(final);
+        }
+	}
+}
---
+++ Assets/Scripts/MaterialSetter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MaterialSetter : MonoBehaviour {
+
+    public Material pureGold;
+    public Material midScratchedGold;
+    public Material damagedGold;
+
+    public Dropdown dropDown;
+
+    public enum MaterialType {
+        PureGold = 0,
+        MidScratchedGold = 1,
+        DamagedGold = 2,
+    }
+
+    private void Awake() {
+        SetMaterial((MaterialType)dropDown.value);
+        dropDown.onValueChanged.AddListener((int value) => { SetMaterial((MaterialType)value); });
+    }
+
+    private void SetMaterial(MaterialType materialType) {
+        var renderer = this.gameObject.GetComponent<Renderer>();
+
+        Material chosenMat = pureGold;
+        switch (materialType) {
+            case MaterialType.PureGold:
+                chosenMat = pureGold;
+                break;
+            case MaterialType.MidScratchedGold:
+                chosenMat = midScratchedGold;
+                break;
+            case MaterialType.DamagedGold:
+                chosenMat = damagedGold;
+                break;
+        }
+
+        if (renderer) {
+            renderer.material = chosenMat;
+        }
+    }
+}
